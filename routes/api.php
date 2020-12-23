61173648f8249327977adaa89c7d89fb544ab1ab
albert-marimon <info@albertmarimon.com>
2020-12-23 09:17:35 +0100

    Rutas API

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//Rutas del controlador de usuario
+Route::resource('user', 'App\Http\Controllers\UserController');
+Route::post('/login','App\Http\Controllers\UserController@login');
+//Route::put('/user/update','App\Http\Controllers\UserController@update');
+//Route::post('/user/upload','App\Http\Controllers\UserController@upload')->middleware('api.auth');
+//Route::get('/user/avatar/{filename}','App\Http\Controllers\UserController@getImage');
